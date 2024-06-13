8047b591fa32344ea18b39e3f3189ab24c71a4d9
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2024-06-13 09:11:38 -0600

    Proyecto web

---
+++ main.js
@@ -0,0 +1,13 @@
+document.getElementById("ageForm").addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevenir el envío del formulario
+
+  const age = parseInt(document.getElementById("ageInput").value);
+
+  if (isNaN(age)) {
+    alert("Por favor, ingrese una edad válida.");
+  } else if (age < 15) {
+    alert("¡Felicidades! Tienes un descuento.");
+  } else {
+    alert("Lo siento, no tienes descuento.");
+  }
+});
